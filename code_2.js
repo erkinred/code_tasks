90554a3203c5a842cc74fb4abc3088b9ebd1f523
Erkin Tilavberdiyev <80733752+erkinred@users.noreply.github.com>
2022-07-27 03:45:09 +0600

    code_2

---
+++ code_2.js
@@ -0,0 +1,11 @@
+const storeName = "Erkin";
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['Azamat', 'Dauren', 'Ramazan'],
+    products: {
+        cola: 200,
+        pepsi: 220
+    },
+    open: true
+}
