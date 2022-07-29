77b4483daa4810b13e2a05ff6f96bac9ea13a93b
Erkin Tilavberdiyev <80733752+erkinred@users.noreply.github.com>
2022-07-29 17:24:45 +0600

    code_4.1

---
+++ code_4.js
@@ -0,0 +1,12 @@
+//1
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    console.log(result);
+    
+    return result;
+}
